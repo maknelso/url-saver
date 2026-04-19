1c777afedaf6d10186265dce2bfaef1c7dfa70fe
Nelson Mak <55319541+maknelso@users.noreply.github.com>
2026-04-19 12:16:51 -0700

    add import for ActiveProfiles

--- src/test/java/com/nelson/urlsaver/UrlSaverApplicationTests.java
+++ src/test/java/com/nelson/urlsaver/UrlSaverApplicationTests.java
@@ -1,6 +1,7 @@
 package com.nelson.urlsaver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @ActiveProfiles("test")
--- src/test/java/com/nelson/urlsaver/controller/UrlControllerTest.java
+++ src/test/java/com/nelson/urlsaver/controller/UrlControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+import org.springframework.test.context.ActiveProfiles;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
